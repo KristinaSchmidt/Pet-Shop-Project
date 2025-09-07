531b0600fd0cfc7eabe76339f24d80fcc1cf0768
Kristina Schmidt <kristina_schmidt21@web.de>
2025-09-07 11:13:22 +0200

    Footer finish

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,50 @@
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
+import mapImage from "../../assets/images/map.png";
+import instagramIcon from "../../assets/icons/ic-instagram.svg";
+import whatsappIcon from "../../assets/icons/ic-whatsapp.svg";
+
 function Footer() {
-    return <p>Footer</p>
+    return (
+    <div className={styles.footer}>
+      <h1>Contact</h1>
+
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <p>Phone</p>
+          <h2>+49 30 915-88492</h2>
+        </div>
+
+        <div className={styles.card}>
+          <p>Socials</p>
+          <div className={styles.socials}>
+            <Link to="/">
+              <img src={instagramIcon} alt="Instagram" />
+            </Link>
+            <Link to="/">
+              <img src={whatsappIcon} alt="WhatsApp" />
+            </Link>
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <p>Address</p>
+          <h2>Wallstraße 9-13, 10179 Berlin, Deutschland</h2>
+        </div>
+
+
+        <div className={styles.card}>
+          <p>Working Hours</p>
+          <h2>24 hours a day</h2>
+        </div>
+      </div>
+
+
+      <div className={styles.map}>
+        <img src={mapImage} alt="Map" />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
